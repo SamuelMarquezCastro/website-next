236566fac84ed71d17467bfb01efeecf3282992d
Samuel Marquez Castro <r1042674@student.thomasmore.be>
2026-05-18 12:27:42 +0200

    first commit

---
+++ data/database.js
@@ -0,0 +1,150 @@
+{
+    "config": {
+        "siteTitle": "Next",
+        "siteLang": "en",
+        "adminLang": "en",
+        "theme": "sky",
+        "defaultPage": "home",
+        "login": "admin",
+        "forceLogout": false,
+        "forceHttps": false,
+        "saveChangesPopup": false,
+        "modalPersistence": false,
+        "logoutToLoginScreen": true,
+        "password": "$2y$10$g208zimWcMljnJLVJv.OL.WMTBB7XVKy0CKeI.rj.uYezZUgjvfAC",
+        "lastLogins": {
+            "2026\/05\/18 12:22:34": "::1",
+            "2026\/05\/18 05:55:59": "193.191.150.44",
+            "2026\/05\/18 05:54:11": "193.191.150.44",
+            "2026\/05\/18 05:53:12": "193.191.150.44",
+            "2026\/05\/18 05:52:41": "193.191.150.44"
+        },
+        "lastModulesSync": "2026\/05\/18",
+        "customModules": {
+            "themes": {},
+            "plugins": {}
+        },
+        "disabledPlugins": {},
+        "menuItems": {
+            "0": {
+                "name": "Home",
+                "slug": "home",
+                "visibility": "show",
+                "subpages": {}
+            },
+            "1": {
+                "name": "Werking",
+                "slug": "werking",
+                "visibility": "show",
+                "subpages": {}
+            },
+            "2": {
+                "name": "Visie",
+                "slug": "visie",
+                "visibility": "show",
+                "subpages": {}
+            },
+            "3": {
+                "name": "Voor wie",
+                "slug": "voor-wie",
+                "visibility": "show",
+                "subpages": {}
+            },
+            "4": {
+                "name": "Over ons",
+                "slug": "over-ons",
+                "visibility": "show",
+                "subpages": {}
+            },
+            "5": {
+                "name": "Contact",
+                "slug": "contact",
+                "visibility": "show",
+                "subpages": {}
+            }
+        }
+    },
+    "pages": {
+        "404": {
+            "created": "2026-04-30T11:26:15-04:00",
+            "modified": "2026-04-30T11:26:15-04:00",
+            "visibility": "show",
+            "title": "404",
+            "keywords": "404",
+            "description": "404",
+            "content": "<center><h1>404 - Page not found<\/h1><\/center>",
+            "subpages": {}
+        },
+        "home": {
+            "created": "2026-04-30T11:26:15-04:00",
+            "modified": "2026-05-18T05:06:46-04:00",
+            "visibility": "show",
+            "title": "Home",
+            "keywords": "Enter, page, keywords, for, search, engines",
+            "description": "A page description is also good for search engines.",
+            "content": "\n\n\n  <meta charset=\"UTF-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\n  <title>NEXT<\/title>\n\n  <link rel=\"stylesheet\" href=\"css\/normalize.css\">\n  <link rel=\"stylesheet\" href=\"css\/style.css\">\n\n\n\n  <header class=\"site-header\">\n    <a href=\"index.html\" class=\"site-logo\">NEXT<\/a>\n\n    <nav class=\"site-nav\">\n      <a href=\"#\">Home<\/a>\n      <a href=\"#\">Werking<\/a>\n      <a href=\"#\">Visie<\/a>\n      <a href=\"#\">Voor wie<\/a>\n      <a href=\"#\">Over ons<\/a>\n      <a href=\"#\">Aanmelden<\/a>\n      <a href=\"#\">Contact<\/a>\n    <\/nav>\n  <\/header>\n\n  <main>\n    <section class=\"hero\">\n      <p>welkom bij NEXT<\/p>\n      <h1>Groeien op jouw tempo<\/h1>\n    <\/section>\n\n    <section class=\"intro\">\n      <h2>De volgende stap, op jouw tempo<\/h2>\n\n      <p>\n        NEXT is een neutrale groeiplek voor jongeren. Hier ben je terecht voor rust,\n        verbinding en persoonlijke ontwikkeling.\n      <\/p>\n\n      <a href=\"#\">Ontdek wat we doen<\/a>\n    <\/section>\n  <\/main>\n\n  <footer class=\"site-footer\">\n    <p>© 2026 NEXT<\/p>\n  <\/footer>\n\n\n",
+            "subpages": {}
+        },
+        "werking": {
+            "created": "2026-04-30T11:35:07-04:00",
+            "modified": "2026-05-18T04:56:07-04:00",
+            "title": "Werking",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "A short description is also good.",
+            "subpages": {},
+            "visibility": "show",
+            "content": "<h2>Werking<\/h2>"
+        },
+        "visie": {
+            "created": "2026-04-30T11:35:48-04:00",
+            "modified": "2026-04-30T11:36:00-04:00",
+            "title": "Visie",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "A short description is also good.",
+            "subpages": {},
+            "visibility": "show",
+            "content": "<h2>Visie<\/h2>"
+        },
+        "voor-wie": {
+            "created": "2026-04-30T11:36:22-04:00",
+            "modified": "2026-04-30T11:36:43-04:00",
+            "title": "Voor Wie",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "A short description is also good.",
+            "subpages": {},
+            "visibility": "show",
+            "content": "<h2>Voor wie<\/h2>"
+        },
+        "over-ons": {
+            "created": "2026-05-11T07:58:44-04:00",
+            "modified": "2026-05-11T07:59:16-04:00",
+            "title": "Over Ons",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "A short description is also good.",
+            "subpages": {},
+            "visibility": "show",
+            "content": "<h2>Over ons<\/h2>"
+        },
+        "contact": {
+            "created": "2026-05-11T07:59:32-04:00",
+            "modified": "2026-05-11T07:59:47-04:00",
+            "title": "Contact",
+            "keywords": "Keywords, are, good, for, search, engines",
+            "description": "A short description is also good.",
+            "subpages": {},
+            "visibility": "show",
+            "content": "<h2>Contact<\/h2>"
+        }
+    },
+    "blocks": {
+        "subside": {
+            "content": "<h2>About your website???<\/h2><p><br><\/p>\n\n<br>\n<p>Website description, contact form, mini map or anything else.<\/p>\n<p>This editable area is visible on all pages.<\/p>"
+        },
+        "header": {
+            "content": "<nav>You can include this in your theme.php and edit it.<\/nav>"
+        },
+        "footer": {
+            "content": "©2026 NEXT."
+        }
+    }
+}
